16ff9b98f5e061b24049418d4a034c4663ce71a6
Shinkudaisuki <3535576702@qq.com>
2023-10-08 12:10:22 +0800

    add phoneVerification field

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -38,6 +38,7 @@ export default {
         password: '',
         confirmPassword: '',
         phoneNumber: '',
+        phoneVerification: '',
         verified: false,
         token: '',
         verify: '',
