d8ce7c859cf9419339f01da32f1163b95bee691a
wxwbeyond <1832072677@qq.comx>
2023-11-02 08:06:39 +0800

    add projectdetail

---
+++ src/views/ProjectDetail.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-card>
+      <h2>项目详情</h2>
+      <p>项目编号: {{ project.ProjectID }}</p>
+      <p>标题: {{ project.Title }}</p>
+      <p>描述: {{ project.Description }}</p>
+      <p>目标金额: {{ project.GoalAmount }}</p>
+      <p>已募集金额: {{ project.RaisedAmount }}</p>
+      <p>审核状态: {{ project.Status }}</p>
+      <p>商家名称: {{ project.MerchantName }}</p>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      project: null,
+    };
+  },
+  mounted() {
+    const projectId = this.$route.params.projectId;
+    // 使用POST请求从后端获取特定项目的详细信息
+    axios.post('/query/projectitems', { projectId: projectId })
+      .then(response => {
+        this.project = response.data; 
+      })
+      .catch(error => {
+        console.error('获取项目详情失败', error);
+      });
+  },
+};
+</script>
