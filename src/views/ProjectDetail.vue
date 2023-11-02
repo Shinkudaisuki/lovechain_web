f160279a7ee92b1e92c49d0bbb97ce2f3fc02281
wxwbeyond <1832072677@qq.comx>
2023-11-02 20:10:48 +0800

    add resp.data

--- src/views/ProjectDetail.vue
+++ src/views/ProjectDetail.vue
@@ -2,13 +2,13 @@
   <div>
     <el-card>
       <h2>项目详情</h2>
-      <p>项目编号: {{ project.ProjectID }}</p>
-      <p>标题: {{ project.Title }}</p>
-      <p>描述: {{ project.Description }}</p>
-      <p>目标金额: {{ project.GoalAmount }}</p>
-      <p>已募集金额: {{ project.RaisedAmount }}</p>
-      <p>审核状态: {{ project.Status }}</p>
-      <p>商家名称: {{ project.MerchantName }}</p>
+      <p>项目编号: {{ resp.data.ProjectID }}</p>
+      <p>标题: {{ resp.data.Title }}</p>
+      <p>描述: {{ resp.data.Description }}</p>
+      <p>目标金额: {{ resp.data.TargetAmount }}</p>
+      <p>已募集金额: {{ resp.data.RaisedAmount }}</p>
+      <p>审核状态: {{ resp.data.ApprovalStatus }}</p>
+      <p>商家名称: {{ resp.data.bus_name.MerchantName }}</p>
     </el-card>
   </div>
 </template>
@@ -19,7 +19,7 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      project: null
+      resp: undefined
     }
   },
   mounted() {
@@ -27,7 +27,7 @@ export default {
     // 使用POST请求从后端获取特定项目的详细信息
     axios.post('/query/projectdetails',{ ProjectTitle: ProjectTitle })
       .then(resp => {
-        this.project = resp; 
+        this.resp = resp; 
       })
       .catch(error => {
         console.error('获取项目详情失败', error);
